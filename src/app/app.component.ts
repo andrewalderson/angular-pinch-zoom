b30c9a22678d7a5b3c97193f88c1964768c5effe
Andrew Alderson <andrew.alderson@outlook.com>
2023-02-24 12:54:27 -0700

    chore: remove default NX component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,9 @@
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { Component } from '@angular/core';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent],
   selector: 'angular-pinch-zoom-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-pinch-zoom';
-}
+export class AppComponent {}
