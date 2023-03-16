a23875165b3cee5c2a7e33f8a04122ee66a8bdb7
Andrew Alderson <andrew.alderson@outlook.com>
2023-03-15 19:12:26 -0600

    add initial ng cli application

---
+++ projects/angular-pinch-zoom/src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "ama-root",
+  standalone: true,
+  template: `<p>App works!</p>`,
+  styles: [],
+})
+export class AppComponent {}
